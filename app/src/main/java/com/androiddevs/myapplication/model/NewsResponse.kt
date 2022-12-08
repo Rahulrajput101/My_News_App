8323616426925843e7c17477210919d7be606428
rahul <itsmerahul10534952@gmail.com>
2022-12-08 19:03:02 +0530

    Revert "news app"
    This reverts commit 54d1767c0c9ec644963333623b817c1810ab8cfd.

--- app/src/main/java/com/androiddevs/myapplication/model/NewsResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.androiddevs.myapplication.model
-
-data class NewsResponse(
-    val articles: List<Article>,
-    val status: String,
-    val totalResults: Int
-)
--- app/src/main/java/com/androiddevs/myapplication/model/Source.kt
+++
@@ -1,6 +0,0 @@
-package com.androiddevs.myapplication.model
-
-data class Source(
-    val id: String,
-    val name: String
-)
